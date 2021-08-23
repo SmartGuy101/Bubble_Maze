17eeee0a0b817d45bcf8914ff54412b7472f7033
Daniel <74032978+SmartGuy101@users.noreply.github.com>
2021-08-23 11:30:26 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`End Tile`, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+info.setLife(3)
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(13)
 let MyPlayer = sprites.create(img`
@@ -21,3 +25,11 @@ let MyPlayer = sprites.create(img`
 tiles.placeOnTile(MyPlayer, tiles.getTileLocation(1, 1))
 controller.moveSprite(MyPlayer)
 scene.cameraFollowSprite(MyPlayer)
+forever(function () {
+    if (MyPlayer.tileKindAt(TileDirection.Center, assets.tile`Spike2`)) {
+        info.changeLifeBy(-1)
+        MyPlayer.y += -25
+        scene.cameraShake(4, 500)
+        MyPlayer.say("!OOF!", 3000)
+    }
+})
